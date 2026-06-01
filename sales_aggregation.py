571e8635ee44fa753542d369b6d1cd0fe49798ea
artgitb0tek <162246154+artgitb0tek@users.noreply.github.com>
2026-05-31 20:50:25 -0400

    sparkfinops: apply cost optimisation

--- sales_aggregation.py
+++ sales_aggregation.py
@@ -10,8 +10,8 @@ large_df = spark.read.csv("path/to/large_file.csv", header=True, inferSchema=Tru
 # Load small DataFrame
 small_df = spark.read.csv("path/to/small_file.csv", header=True, inferSchema=True)
 
-# Perform join operation with broadcast
-result_df = large_df.join(broadcast(small_df), on="join_key", how="inner")
+# Optimize join operation using broadcast hint
+result_df = large_df.join(broadcast(small_df), on="join_column", how="inner")
 
 # Show result
 result_df.show()
