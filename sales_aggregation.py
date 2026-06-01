992db41fd7215119ad09771945b3ce367f46c966
artgitb0tek <162246154+artgitb0tek@users.noreply.github.com>
2026-05-31 20:48:52 -0400

    sparkfinops: apply cost optimisation

--- sales_aggregation.py
+++ sales_aggregation.py
@@ -1,28 +1,18 @@
 ```python
 from pyspark import SparkContext
 from pyspark.sql import SparkSession
+from pyspark.sql.functions import broadcast
 
 sc = SparkContext.getOrCreate()
 spark = SparkSession(sc)
 
-# Sample data
-data1 = [("a", 1), ("b", 2), ("a", 3), ("b", 4)]
-data2 = [("a", 5), ("b", 6)]
+# Load data
+large_rdd = sc.textFile("path/to/large_file.txt")
+small_rdd = sc.textFile("path/to/small_file.txt")
 
-# Create RDDs
-rdd1 = sc.parallelize(data1)
-rdd2 = sc.parallelize(data2)
+# Perform join operation with broadcasting
+small_rdd_broadcasted = broadcast(small_rdd.collectAsMap())
+joined_rdd = large_rdd.map(lambda x: (x.split(",")[0], x)).join(small_rdd_broadcasted.value)
 
-# Use reduceByKey instead of groupByKey
-reduced_rdd = rdd1.reduceByKey(lambda x, y: x + y)
-
-# Broadcast the smaller RDD for the join
-broadcast_rdd2 = sc.broadcast(rdd2.collectAsMap())
-
-# Perform join using the broadcasted variable
-joined_rdd = reduced_rdd.map(lambda x: (x[0], (x[1], broadcast_rdd2.value.get(x[0], 0))))
-
-# Collect results
-results = joined_rdd.collect()
-print(results)
+# Further processing...
 ```
