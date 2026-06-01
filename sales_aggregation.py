23865f4d8e3e37ff4d4c7979bf2526c907120076
artgitb0tek <162246154+artgitb0tek@users.noreply.github.com>
2026-05-31 20:48:31 -0400

    sparkfinops: apply cost optimisation

--- sales_aggregation.py
+++ sales_aggregation.py
@@ -1,13 +1,28 @@
-# sales_aggregation.py
+```python
+from pyspark import SparkContext
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import broadcast
 
-spark = SparkSession.builder.appName("SalesAgg").getOrCreate()
+sc = SparkContext.getOrCreate()
+spark = SparkSession(sc)
 
-# Bad version: no broadcast hint, groupByKey instead of reduceByKey
-orders = spark.read.parquet("s3://bucket/orders/")
-customers = spark.read.parquet("s3://bucket/customers/")
-joined = orders.join(customers, "customer_id", "inner")
-rdd = joined.rdd.map(lambda row: (row["product_id"], row["amount"]))
-result = rdd.groupByKey().mapValues(sum).collect()
-print(result)
+# Sample data
+data1 = [("a", 1), ("b", 2), ("a", 3), ("b", 4)]
+data2 = [("a", 5), ("b", 6)]
+
+# Create RDDs
+rdd1 = sc.parallelize(data1)
+rdd2 = sc.parallelize(data2)
+
+# Use reduceByKey instead of groupByKey
+reduced_rdd = rdd1.reduceByKey(lambda x, y: x + y)
+
+# Broadcast the smaller RDD for the join
+broadcast_rdd2 = sc.broadcast(rdd2.collectAsMap())
+
+# Perform join using the broadcasted variable
+joined_rdd = reduced_rdd.map(lambda x: (x[0], (x[1], broadcast_rdd2.value.get(x[0], 0))))
+
+# Collect results
+results = joined_rdd.collect()
+print(results)
+```
