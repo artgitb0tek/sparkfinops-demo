552800692e166dce00e035cd61b3537c5b1162bb
artgitb0tek <162246154+artgitb0tek@users.noreply.github.com>
2026-05-31 20:49:18 -0400

    sparkfinops: apply cost optimisation

--- sales_aggregation.py
+++ sales_aggregation.py
@@ -1,18 +1,29 @@
 ```python
 from pyspark import SparkContext
+from pyspark import SparkConf
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import broadcast
+from pyspark.sql import functions as F
 
-sc = SparkContext.getOrCreate()
+conf = SparkConf().setAppName("OptimizationExample")
+sc = SparkContext(conf=conf)
 spark = SparkSession(sc)
 
 # Load data
-large_rdd = sc.textFile("path/to/large_file.txt")
-small_rdd = sc.textFile("path/to/small_file.txt")
+small_rdd = sc.textFile("small_data.txt")
+large_rdd = sc.textFile("large_data.txt")
 
-# Perform join operation with broadcasting
-small_rdd_broadcasted = broadcast(small_rdd.collectAsMap())
-joined_rdd = large_rdd.map(lambda x: (x.split(",")[0], x)).join(small_rdd_broadcasted.value)
+# Broadcast the small RDD
+broadcasted_small_rdd = sc.broadcast(small_rdd.collectAsMap())
 
-# Further processing...
+# Join operation using broadcasted variable
+joined_rdd = large_rdd.map(lambda x: (x[0], x)).join(broadcasted_small_rdd.value)
+
+# Use reduceByKey for aggregation
+result_rdd = joined_rdd.map(lambda x: (x[0], x[1][0], x[1][1])) \
+                        .reduceByKey(lambda a, b: (a[0], a[1] + b[1]))
+
+# Save the result
+result_rdd.saveAsTextFile("output.txt")
+
+sc.stop()
 ```
